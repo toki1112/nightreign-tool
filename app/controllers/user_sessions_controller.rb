2461253ec26b42e7eadc88a22e46558c5ada59f4
tokishh <toki1112bass@gmail.com>
2026-06-28 12:58:34 +0900

    ログインとログアウト機能の実装

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,21 @@
 class UserSessionsController < ApplicationController
   def new
+
+  end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_to root_path, notice: "ログインしました"
+    else
+      flash.now[:alert] = "ログインに失敗しました"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, notice: "ログアウトしました", status: :see_other
   end
 end
